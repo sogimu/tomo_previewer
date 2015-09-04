a28a88756c9e7531049df8df0c7e93d79ecaea6e
Aleksandr Lizin <sogimu@nxt.ru>
2015-09-04 22:46:13 +0100

    Fixed crone task

--- schedule_slices_processing.sh
+++ schedule_slices_processing.sh
@@ -3,7 +3,8 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "01 * * * * trigger_slices_processing.py run >> slices_processing.log" >> mycron
+echo "01 * * * * $(pwd)/trigger_slices_processing.py run >> $(pwd)/slices_processing.log" >> mycron
+
 #install new cron file
 crontab mycron
 rm mycron
